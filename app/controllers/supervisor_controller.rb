ebea764a5d984d1ece439f0d52d6e421ea9d0f45
LucaBla <me@lucablazevic.dev>
2023-05-31 22:11:17 +0200

    add supervisor@show

--- app/controllers/supervisor_controller.rb
+++ app/controllers/supervisor_controller.rb
@@ -1,11 +1,9 @@
 class SupervisorController < ApplicationController
   skip_before_action :verify_authenticity_token, raise: false  
-  #before_action :authenticate_devise_api_token!
+  before_action :authenticate_devise_api_token!
   #before_action :set_theme, only: [:show, :update, :destroy]
   
   def index
-    puts 'test'
-    puts params[:ids] == ['']
     if params[:ids].present? && params[:ids] != ['']
       #@supervisors = Supervisor.all.joins(:tags).where(tags: { id: [1, 2] })
       @supervisor = Supervisor.all.joins(:tags).where(tags: { id: params[:ids] }).distinct
@@ -14,5 +12,12 @@ class SupervisorController < ApplicationController
     end
 
     render json: @supervisor, include: :tags
+  end
+
+  def show
+    if(current_devise_api_token.resource_owner.type == 'Supervisor')
+      render json: current_devise_api_token.resource_owner, include: :tags, status: :ok
+    else render json: 'test', status: :ok
+    end
   end
 end
