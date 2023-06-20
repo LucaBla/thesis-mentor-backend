5b9046e94bf7d9d179a4fc0de0d6f1009ccf1fbc
LucaBla <me@lucablazevic.dev>
2023-06-20 15:41:26 +0200

    Try to create already existing chat now opens this chat

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -25,14 +25,21 @@ class ChatsController < ApplicationController
 
   # POST /chats
   def create
-    @chat = Chat.new(chat_params)
+    @chat = Chat.where(supervisor_id: chat_params[:supervisor_id], 
+                       student: current_devise_api_token.resource_owner,
+                       theme_id: chat_params[:theme_id])
+    if @chat == []
+      @chat = Chat.new(chat_params)
 
-    @chat.student = current_devise_api_token.resource_owner
+      @chat.student = current_devise_api_token.resource_owner
 
-    if @chat.save
-      render json: @chat, status: :created, location: @chat
-    else
-      render json: @chat.errors, status: :unprocessable_entity
+      if @chat.save
+        render json: @chat, status: :created, location: @chat
+      else
+        render json: @chat.errors, status: :unprocessable_entity
+      end
+    else  
+      render json: @chat.first
     end
   end
 
